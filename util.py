40b68d29e0d4d55374fe26846b01cbe89cad382c
BOBO <54936491+Luciferbobo@users.noreply.github.com>
2022-04-29 20:11:09 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+import torch
 import logging
+import math
 import torch.nn as nn
+
+alpha,beta=0.6,0.4
 
 def get_logger(filepath, log_info):
     logger = logging.getLogger(filepath)
@@ -15,8 +22,8 @@ def log_and_print(logger, msg):
     logger.info(msg)
     print(msg)
 
-def loss_function(recon_x, x, mu):
+def loss_function(sigma, x, mu):
     MSE_loss = nn.MSELoss(reduction='sum')
-    reconstruction_loss = MSE_loss(recon_x, x)
-    #reconstruction_loss = MSE_loss(recon_x, x)+MSE_loss(x, mu)
-    return reconstruction_loss
+    rec_loss = alpha/((sigma**2)*MSE_loss(x, mu))
+    sup_loss = beta*math.log(sigma**2)
+    return rec_loss+sup_loss
